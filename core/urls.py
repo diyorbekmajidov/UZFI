26dec0f94012e81dd7f3ee0eae3f0ddf86a61299
Diyorbek <majidovdiyorbek741@gmail.com>
2023-09-19 10:21:45 +0500

    translation app models

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('uzfi/', include('UZFI.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+]+i18n_patterns (
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('uzfi/', include('UZFI.urls')),
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
