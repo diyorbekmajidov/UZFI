429f5cd493ce29660ac7515feec723da09e7ccc6
Diyorbek <majidovdiyorbek741@gmail.com>
2023-09-19 11:55:06 +0500

    charter vies create

--- UZFI/urls.py
+++ UZFI/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 from . import views
 from .views import (
-    CharterListCreate
+    CharterListCreate,
+    CharterRetrieveUpdate,
+    DocumentCreate,
+    DocumentRetrieveUpdate,
+    CouncilsListCreate,
+    CouncilsRetrieveUpdate
 )
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('charter/', CharterListCreate.as_view(), name='charter')
+    path('charter/', CharterListCreate.as_view(), name='charter'),
+    path('charter/<int:pk>/', CharterRetrieveUpdate.as_view(), name='charter-detail'),
+
+    path('document/', DocumentCreate.as_view(), name='document'),
+    path('document/<int:pk>/', DocumentRetrieveUpdate.as_view(), name='document-detail'),
+
+    path('councils/', CouncilsListCreate.as_view(), name='councils'),
+    path('councils/<int:pk>/',CouncilsRetrieveUpdate.as_view() , name='council'),
 ]
--- UZFI/views/views.py
+++ UZFI/views/views.py
@@ -6,9 +6,27 @@ from UZFI.models import *
 from UZFI.serializers import *
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-def home(request):
-    return render("vhxkbjnflam")
 
 class CharterListCreate(ListCreateAPIView):
     queryset = Charter.objects.all()
     serializer_class = CharterSerializer
+
+class CharterRetrieveUpdate(RetrieveUpdateDestroyAPIView):
+    queryset = Charter.objects.all()
+    serializer_class = CharterSerializer
+
+class DocumentCreate(ListCreateAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+class DocumentRetrieveUpdate(RetrieveUpdateDestroyAPIView):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+class CouncilsListCreate(ListCreateAPIView):
+    queryset = Councils.objects.all()
+    serializer_class = CouncilsSerializer
+
+class CouncilsRetrieveUpdate(RetrieveUpdateDestroyAPIView):
+    queryset = Councils.objects.all()
+    serializer_class = CouncilsSerializer
