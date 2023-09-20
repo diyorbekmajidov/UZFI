0b77819d3bceff61dd084ea5644947b70aef2d2e
Diyorbek <majidovdiyorbek741@gmail.com>
2023-09-20 14:31:31 +0500

    faculty models create

--- UZFI/models.py
+++ UZFI/models.py
@@ -32,8 +32,6 @@ class Councils(models.Model):
 
     def __str__(self):
         return self.title
-    
-
 
 class Requisites(models.Model):
     unversit_name = models.CharField(max_length=100)
@@ -77,6 +75,68 @@ class Vacancies(models.Model):
     department   = RichTextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_update  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+    body = RichTextUploadingField()
+
+    def __str__(self):
+        return self.name
+    
+class Dekan(models.Model):
+    faculty      = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    name         = models.CharField(max_length=100)
+    email        = models.CharField(max_length=100)
+    phone        = models.CharField(max_length=100)
+    acceptance   = models.CharField(max_length=200)
+    address      = models.CharField(max_length=100)
+    img          = models.ImageField(upload_to='img/')
+    duties       = RichTextUploadingField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Kafedra(models.Model):
+    faculty      = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    name         = models.CharField(max_length=100)
+    about        = RichTextUploadingField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class KafedraManager(models.Model):
+    kafedra      = models.ForeignKey(Kafedra, on_delete=models.CASCADE)
+    name         = models.CharField(max_length=100)
+    email        = models.CharField(max_length=100)
+    phone        = models.CharField(max_length=100)
+    acceptance   = models.CharField(max_length=200)
+    address      = models.CharField(max_length=100)
+    img          = models.ImageField(upload_to='img/')
+    duties       = RichTextUploadingField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update  = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class KafedraTeacher(models.Model):
+    kafedra         = models.ForeignKey(Kafedra, on_delete=models.CASCADE)
+    name            = models.CharField(max_length=100)
+    email           = models.CharField(max_length=100)
+    phone           = models.CharField(max_length=100)
+    address         = models.CharField(max_length=100)
+    img             = models.ImageField(upload_to='img/')
+    biography       = RichTextUploadingField()
+    date_created    = models.DateTimeField(auto_now_add=True)
+    date_update     = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
