47defdca0a532970e32bcc45e6035ece8d2f3d2f
Diyorbek <majidovdiyorbek741@gmail.com>
2023-09-27 14:23:24 +0500

    news models create

--- News/models.py
+++ News/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
 # Create your models here.
+from UZFI.models import Dekan
+
+class NewsCategory(models.Model):
+    new_category = models.CharField(max_length=255,unique=True)
+
+    def __str__(self) -> str:
+        return self.new_category
+    
+class News_Content(models.Model):
+    yangiliklar   = models.ManyToManyField(NewsCategory)
+    dekan         = models.ForeignKey(Dekan, on_delete=models.CASCADE, blank=True, null=True)
+    title         = models.CharField(max_length=255)
+    body          = RichTextUploadingField()
+    views         = models.IntegerField(default=0)
+    date_created  = models.DateTimeField(auto_now_add=True)
+    date_update   = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
