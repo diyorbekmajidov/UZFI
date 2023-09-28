5d603f3e89c4b68d1a7b2e08987bf32391c5b498
Diyorbek <majidovdiyorbek741@gmail.com>
2023-09-28 10:02:59 +0500

    news catogry serializers' git push

--- News/serializers.py
+++ News/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import *
+
+
+class NewsCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsCategory
+        fields = '__all__'
