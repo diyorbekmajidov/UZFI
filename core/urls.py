55db27be6641e297daceec44251f2e2057110adb
Diyorbek <majidovdiyorbek741@gmail.com>
2023-09-28 10:01:51 +0500

    add news urls

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 ]+i18n_patterns (
     path('i18n/', include('django.conf.urls.i18n')),
     path('uzfi/', include('UZFI.urls')),
+    path('news/', include('News.urls'))
 )
 
 if settings.DEBUG:
