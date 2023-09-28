51814714d26d64a930c4a937fb4a32374f323f6f
Diyorbek <majidovdiyorbek741@gmail.com>
2023-09-28 16:11:14 +0500

    git news by id create views

--- News/urls.py
+++ News/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from .views import (
     NewsCategoryListCreate,
-    NewsContentApiview
+    NewsContentApiview,
+    NewsContentApiviewGet,
 )
 
 urlpatterns = [
     path('category/', NewsCategoryListCreate.as_view(), name='news_category_list_create'),
-    path('content/', NewsContentApiview.as_view(), name='news_content_list_create')
+    path('content/', NewsContentApiview.as_view(), name='news_content_list_create'),
+    path('content/<int:pk>/', NewsContentApiviewGet.as_view(), name='news_content_list_create')
 ]
--- News/views.py
+++ News/views.py
@@ -24,3 +24,26 @@ class NewsContentApiview(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class NewsContentApiviewGet(APIView):
+    def get(self, request, pk):
+        news_content = News_Content.objects.get(pk=pk)
+        views = news_content.views+1
+        news_content.views = views
+        news_content.save()
+        serializer = NewsContentSerializer(news_content)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        news_content = News_Content.objects.get(pk=pk)
+        serializer = NewsContentSerializer(news_content, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        news_content = News_Content.objects.get(pk=pk)
+        news_content.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
