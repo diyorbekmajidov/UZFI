fe58c4c24b293d332e0a9b106c4e3f2b9920d4b8
Diyorbek <majidovdiyorbek741@gmail.com>
2023-10-06 14:51:08 +0500

    get all dekan views create

--- UZFI/views/dekanview.py
+++ UZFI/views/dekanview.py
@@ -1,13 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from UZFI.models.models import *
+from UZFI.models.models import Dekan
 from UZFI.serializers import *
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+class AllDekan(ListCreateAPIView):
+    queryset = Dekan.objects.all()
+    serializer_class = GetDekanSerializer
+
 
 class DekanApiview(APIView):
     @swagger_auto_schema(
@@ -34,6 +40,8 @@ class DekanApiview(APIView):
                     return Response(serializer.data, status=status.HTTP_201_CREATED)
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             return Response({'error': 'Sizning role noto\'g\'ri'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    
     
 class DekanGetApiview(APIView):
     authentication_classes = [TokenAuthentication]
@@ -65,3 +73,4 @@ class DekanGetApiview(APIView):
         dekan = Dekan.objects.get(dekan=request.user)
         dekan.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
