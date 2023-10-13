7ffee0c676a17f062b23ca3c7c3eeb06b6cad302
Azizbek <azizbekqozoqovinfo@gmail.com>
2023-10-13 15:18:02 +0500

    Added homepage

--- static/assets/js/render.js
+++ static/assets/js/render.js
@@ -1,50 +1,48 @@
 const REQUEST_URL = "http://127.0.0.1:8000";
 const paths = {
-    'directions': '/uzfi/direction/',
-    'charter': '/uzfi/charter/',
-    'documents': '/uzfi/document/',
-    'councils':'/uzfi/councils/'
-}
-
+  directions: "/uzfi/direction/",
+  charter: "/uzfi/charter/",
+  documents: "/uzfi/document/",
+  councils: "/uzfi/councils/",
+};
 
 $(document).ready(function () {
-    // render header
-    $(".put-header").load('assets/components/header.html');
-    // render footer
-    $(".put-footer").load('assets/components/footer.html');
-    let profileInfoSessions = ["profileInfoMissions", "profileInfoBio", "profileInfoCompletedMissions"];
-    let profileInfoButtons = ["profileInfoMissionsBTN", "profileInfoBioBTN", "profileInfoCompletedMissionsBTN"];
-    let hideprofileInfoSessions = () => profileInfoSessions.forEach(function (element) { $("#" + element).hide(); });
-    hideprofileInfoSessions();
-    $("#" + profileInfoSessions[0]).show();
-    $("#" + profileInfoButtons[0]).addClass("sessions-active-button");
-    profileInfoButtons.forEach(function (element) {
-
-        $(`#${element}`).click(function (e) {
-            hideprofileInfoSessions();
-            $(`#${$(this).attr('data-for')}`).show();
-            profileInfoButtons.forEach(function (el) {
-                $(`#${el}`).removeClass("sessions-active-button");
-            });
-            $(this).addClass('sessions-active-button');
-
-        });
+  let profileInfoSessions = [
+    "profileInfoMissions",
+    "profileInfoBio",
+    "profileInfoCompletedMissions",
+  ];
+  let profileInfoButtons = [
+    "profileInfoMissionsBTN",
+    "profileInfoBioBTN",
+    "profileInfoCompletedMissionsBTN",
+  ];
+  let hideprofileInfoSessions = () =>
+    profileInfoSessions.forEach(function (element) {
+      $("#" + element).hide();
     });
-
-
-
-
-
-
-})
-
-
+  hideprofileInfoSessions();
+  $("#" + profileInfoSessions[0]).show();
+  $("#" + profileInfoButtons[0]).addClass("sessions-active-button");
+  profileInfoButtons.forEach(function (element) {
+    $(`#${element}`).click(function (e) {
+      hideprofileInfoSessions();
+      $(`#${$(this).attr("data-for")}`).show();
+      profileInfoButtons.forEach(function (el) {
+        $(`#${el}`).removeClass("sessions-active-button");
+      });
+      $(this).addClass("sessions-active-button");
+    });
+  });
+});
 
 // Destinations
-makeRequest(paths['directions'], function (data, statusText) {
+makeRequest(
+  paths["directions"],
+  function (data, statusText) {
     const destinations = $("#destinations .put-destinations");
-    data.forEach(e => {
-        destinations.append(`
+    data.forEach((e) => {
+      destinations.append(`
         <div class="item">
             <div class="cours-bx">
                 <div class="action-box">
@@ -58,15 +56,20 @@ makeRequest(paths['directions'], function (data, statusText) {
                                 
             </div>
         </div>
-        `)
-    })
-}, function (e) { console.log(e) })
-
+        `);
+    });
+  },
+  function (e) {
+    console.log(e);
+  }
+);
 
 // Functions
 function makeRequest(path, done, fail) {
-    $.ajax({
-        'method': 'get',
-        'url': REQUEST_URL + path
-    }).done(done).fail(fail)
+  $.ajax({
+    method: "get",
+    url: REQUEST_URL + path,
+  })
+    .done(done)
+    .fail(fail);
 }
