414ca7b308127c576ba9181597df2f84af836998
Azizbek <azizbekqozoqovinfo@gmail.com>
2023-10-13 14:32:49 +0500

    urls added

--- core/urls.py
+++ core/urls.py
@@ -33,6 +33,9 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # urls for front
+    path('', include('uzfiFront.urls'))
 ]
 urlpatterns=[
     *i18n_patterns(*urlpatterns, prefix_default_language=False),
