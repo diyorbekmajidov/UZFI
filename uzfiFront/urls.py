0fbf04e776114e34d4bb30ef2578d0a2544ee643
Azizbek <azizbekqozoqovinfo@gmail.com>
2023-10-13 14:37:02 +0500

    ready for start

---
+++ uzfiFront/urls.py
@@ -0,0 +1,5 @@
+from .views import Index
+from django.urls import path
+urlpatterns = [
+    path('', Index.as_view())
+]
--- uzfiFront/views.py
+++ uzfiFront/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
 # Create your views here.
+
+
+class Index(View):
+    def get(self, req:HttpRequest, *args, **kwargs):
+        return HttpResponse('Hello')
