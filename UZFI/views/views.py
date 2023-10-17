0413fad1c56ab1627ccf4e6ac487e82de8ad29a1
Diyorbek <majidovdiyorbek741@gmail.com>
2023-10-17 14:49:27 +0500

    11

--- UZFI/views/views.py
+++ UZFI/views/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from UZFI.models.models import *
 from UZFI.serializers import *
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
