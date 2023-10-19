16679c69160b85830469e049dca368911ad36b71
Diyorbek <majidovdiyorbek741@gmail.com>
2023-10-19 19:14:02 +0500

    news category context data

--- News/views.py
+++ News/views.py
@@ -29,36 +29,25 @@ class NewsContentListAPIView(ListView):
     model = News_Content
     paginate_by = 10
     def get(self, request, *args, **kwargs):
-        
+        category = NewsCategory.objects.all()
+        serializer1 = NewsCategorySerializer(category, many = True)
         news_content_list = News_Content.objects.all()
         serializer = NewsContentSerializer(news_content_list, many=True)
-        context = serializer.data
-        return render(request, 'news.html', {"data":serializer.data})
+        return render(request, 'news.html', {
+            "data":serializer.data,
+            "category":serializer1.data,
+                    })
 
     
 
-class NewsContentApiviewGet(APIView):
+class NewsContentApiviewGet(TemplateView):
     def get(self, request, pk):
         news_content = News_Content.objects.get(pk=pk)
         views = news_content.views+1
         news_content.views = views
         news_content.save()
         serializer = NewsContentSerializer(news_content)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        news_content = News_Content.objects.get(pk=pk)
-        serializer = NewsContentSerializer(news_content, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        news_content = News_Content.objects.get(pk=pk)
-        news_content.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
+        return render(request, '.html', {"data":serializer.data,})
 
 class GetUserNews(ListAPIView):
     permission_classes = [IsAuthenticated]
@@ -73,14 +62,6 @@ class GetUserNews(ListAPIView):
         else:
             return News_Content.objects.none()
         
-class GetUserNewsTemplateView(TemplateView):
-    def get_queryset(self):
-        user = self.request.user
-        dekan = Dekan.objects.filter(dekan=user).first()
-        if dekan:
-            return News_Content.objects.filter(dekan=dekan)
-        else:
-            return News_Content.objects.none()
 
 
 class LastNewsApiview(ListAPIView):
