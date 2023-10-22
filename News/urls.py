67f7f4e22cf08423590f5bb42f6bda44e396bd33
Azizbek <azizbekqozoqovinfo@gmail.com>
2023-10-22 23:27:10 +0500

    Some changes for News URLs and VIEWs

--- News/urls.py
+++ News/urls.py
@@ -9,10 +9,15 @@ from .views import (
 )
 
 urlpatterns = [
+
+    # Categories
     path('category/', NewsCategoryListCreate.as_view(), name='news_category_list_create'),
+    path('category/<str:category>/', NewsContentCategoryAPIView.as_view()),
+
+    # Content
     path('content/', NewsContentListAPIView.as_view(), name='news_content_list_create'),
-    path('categoryconntent/<str:category>/', NewsContentCategoryAPIView.as_view()),
     path('content/<int:pk>/', NewsContentApiviewGet.as_view(), name='news_content_list_create'),
+
     path('getusernews/', GetUserNews.as_view(),),
     path('lastnews/', LastNewsApiview.as_view()),
 ]
