803438c2eaca4c647c871beef5348fd8a500e7fc
Diyorbek <majidovdiyorbek741@gmail.com>
2023-10-23 18:00:03 +0500

    leadership render

--- UZFI/views/leadershipapiview.py
+++ UZFI/views/leadershipapiview.py
@@ -10,6 +10,6 @@ class LeadershipAPIView(APIView):
         try :
             leadership = Leadership.objects.all()
             serializers = LeadershipSerializer(leadership, many=True)
-            return render(request, '.html',{"data":serializers.data})
+            return render(request, 'leadership.html',{"data":serializers.data})
         except Exception as e:
             return Response({"ok":[]})
