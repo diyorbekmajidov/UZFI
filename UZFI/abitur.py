c2467e6893a90ba23f312775eaae1b93c9428377
Azizbek <azizbekqozoqovinfo@gmail.com>
2023-11-08 11:57:17 +0500

    .

--- UZFI/abitur.py
+++ UZFI/abitur.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("regulation-on-secondary-education/", RegulationOnSecondaryEducation.as_view()),
     path("work-with-the-answer-sheet/", WorkWithTheAnswerSheet.as_view()),
     path("faqs/", FAQs.as_view()),
-    path("acceptance/", Acceptance.as_view()),
+    path("admission-quota/", Acceptance.as_view()),
     path("commission/", Commission.as_view()),
-    path('regulation/', Regulation.as_view()),
+    path('charter-admission/', Regulation.as_view()),
 ]
