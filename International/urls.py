b5d4003411e31a914c872ab9a4155302679192cf
Diyorbek <majidovdiyorbek741@gmail.com>
2023-11-10 10:09:06 +0500

    11

--- International/urls.py
+++ International/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
+from .views import(
+    InternationalRelationsViews,
+    InternationalMemorandumViews
+)
 
 urlpatterns = [
+    #International Relation views
+    path('internationalrelations/', InternationalRelationsViews.index, name='international_relations'),
+    path('internationalmemorandum/', InternationalMemorandumViews.add, name='add_international_relation'),
     
 ]
