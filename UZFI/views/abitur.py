c7bb8ac257a8df2117948ad279c63c7a759905b8
Azizbek <azizbekqozoqovinfo@gmail.com>
2023-11-20 11:39:26 +0500

    .

--- UZFI/views/abitur.py
+++ UZFI/views/abitur.py
@@ -28,7 +28,7 @@ class Magistratura(TemplateView):
 class Bakalavriar(TemplateView):
     def get(self, request):
         data = Direction.objects.all()
-        serializers = DirectionSerializer(data)
+        serializers = DirectionSerializer(data, many=True)
         template_name = 'bachelor.html'
-        return render(request, '', {"data":serializers.data})
+        return render(request, template_name, {"data":serializers.data})
 
