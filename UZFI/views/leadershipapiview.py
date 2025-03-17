e9c8fec87943d1ec0bfa89ea2642d9415bb11699
Diyorbek <majidovdiyorbek741@gmail.com>
2025-03-17 10:45:46 +0500

    order by numbers

--- UZFI/views/leadershipapiview.py
+++ UZFI/views/leadershipapiview.py
@@ -20,12 +20,8 @@ class LeadershipByIdAPIView(TemplateView):
         try :
             leadership = Leadership.objects.get(id=pk)
             serializers = LeadershipSerializer(leadership)
-            user = leadership.rector.id
-            scientifi_work = ScientificWork.objects.filter(user=user)
-            serializers1 = ScientificWorkSerializer(scientifi_work, many = True)
             return render(request, 'leadership-item.html',{
                 "data":serializers.data,
-                "scientificwork":serializers1.data
                 })
         except Exception as e:
             print(e)
